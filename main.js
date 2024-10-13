3f39314bf1fa7cf445d26de18ff662f83b84fbd6
Deesh Singla <deeshsingla@gmail.com>
2024-10-13 17:45:55 +0530

    Added DOM functionality

--- main.js
+++ main.js
@@ -78,6 +78,7 @@ const GameBoard = (() => {
                 x.textContent = '';
             })
             turn = 0;
+            updatePlayerName();
         })
     }
     const backbtn = () => {
